1d5cc0ee9f0274f26349dc3ebbb4bd9d5f32cab0
Alex Kranias <alexander.kranias@gmail.com>
2022-07-21 17:36:59 -0400

    added method to convert orientation angle to degrees and added comment describing output of getOrientation()

--- app/src/main/java/com/solaign/SensorActivity.java
+++ app/src/main/java/com/solaign/SensorActivity.java
@@ -100,6 +100,17 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
     }
 
+    /*
+    getOrientation() method output:
+
+    THIS IS AZIMUTH ANGLE:  values[0]: Azimuth, angle of rotation about the -z axis. This value represents the angle between the device's y axis and the magnetic north pole. When facing north, this angle is 0, when facing south, this angle is π. Likewise, when facing east, this angle is π/2, and when facing west, this angle is -π/2. The range of values is -π to π.
+    THIS IS THE TILT ANGLE: values[1]: Pitch, angle of rotation about the x axis. This value represents the angle between a plane parallel to the device's screen and a plane parallel to the ground. Assuming that the bottom edge of the device faces the user and that the screen is face-up, tilting the top edge of the device toward the ground creates a positive pitch angle. The range of values is -π/2 to π/2.
+    THIS IS USELESS:        values[2]: Roll, angle of rotation about the y axis. This value represents the angle between a plane perpendicular to the device's screen and a plane perpendicular to the ground. Assuming that the bottom edge of the device faces the user and that the screen is face-up, tilting the left edge of the device toward the ground creates a positive roll angle. The range of values is -π to π.
+
+     i fixed it so it outputs in degrees not radians
+
+    */
+
     // Compute the three orientation angles based on the most recent readings from
     // the device's accelerometer and magnetometer.
     public void updateOrientationAngles() {
@@ -109,8 +120,19 @@ public class SensorActivity extends Activity implements SensorEventListener {
         orientationText.setText(Arrays.toString(accelerometerReading));
         // "rotationMatrix" now has up-to-date information.
 
-        orientationText.setText(Arrays.toString(SensorManager.getOrientation(rotationMatrix, orientationAngles)));
+        float[] orientation = SensorManager.getOrientation(rotationMatrix, orientationAngles);
+        convertOrientationToDegrees(orientation);
+        orientationText.setText(Arrays.toString(orientation));
 
         // "orientationAngles" now has up-to-date information.
     }
-}
+
+    /**
+     * Standard getOrientation outputs in radians. This method converts those values to degrees
+     */
+    private static void convertOrientationToDegrees(float[] orientation) {
+        orientation[0] = (float)Math.toDegrees(orientation[0]);
+        orientation[1] = (float)Math.toDegrees(orientation[1]);
+        orientation[2] = (float)Math.toDegrees(orientation[2]);
+    }
+} //LINK TO getOrientation outputs: https://developer.android.com/reference/android/hardware/SensorManager#getOrientation(float[],%20float[])
